95870fb00e78a4f0427fcff1f62a4b1d9d9cc1d0
po-panda <notcyanpascual@gmail.com>
2022-08-25 11:28:25 +0800

    feat: .env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Setup: npm install alchemy-sdk
 const { Network, Alchemy } = require("alchemy-sdk");
+require("dotenv").config();
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: "dKW2Dz1ZrLoi5HnYNSYXd8SA2SE-FeQU", // Replace with your Alchemy API Key.
+  apiKey: process.env.ALCHEMY_KEY, // Replace with your Alchemy API Key.
   network: Network.ETH_GOERLI, // Replace with your network.
 };
 
